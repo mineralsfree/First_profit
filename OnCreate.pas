a73d074f307fa68ed60062f3cb75761811af5a47
Nikita <mineralsfree@gmail.com>
2018-03-24 22:22:23 +0300

    12
    21

--- OnCreate.pas
+++ OnCreate.pas
@@ -62,10 +62,10 @@ case mode of
   spr1:
     begin
          shoplistkek.id:=StrToInt(InputBox('','','12'));
-         shoplistkek.name=InputBox('','','kek');
-         shoplistkek.adress=InputBox('','','privetlivaya');
-         shoplistkek.tel=InputBox('','','37529235232');
-         insertShopList(sectorhead,shoplistkek);
+         shoplistkek.name:=InputBox('','','kek');
+         shoplistkek.adress:=InputBox('','','privetlivaya');
+         shoplistkek.tel:=InputBox('','','37529235232');
+         insertShopList(shophead,shoplistkek);
         //отрисовать
     end;
   main:
@@ -104,39 +104,90 @@ procedure TForm1.strngrd1MouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
    var i,Acol,Arow:Integer;
    var shopNum,sectnum:Integer;
-   var sectlistkek:TsectorInfo;
+   var sectlistkek:TSectorInfo;
+   var shoplistkek:TShopInfo;
    var getShopID:Integer;
+   var id: Integer;
 
 begin
     case mode of
-     spr1:
+     spr1:       //SHOPLIST
       begin
-        if Acol = 4 then
-          shopNum:=strngrd1.Cells[1,Arow];
-          //deleteShopNum(head,shopNum);
-        if Acol = 5 then
+        case Acol of
+          1:
+            begin
+              id:=StrToInt(strngrd1.Cells[0,Arow]);
+              shoplistkek.id:=id;                //red name
+             shoplistkek.name:=InputBox('','','kek');
+             shoplistkek.adress:=strngrd1.Cells[2,Arow];
+             shoplistkek.tel:=strngrd1.Cells[3,Arow];
+            end;
+          2:
+            begin
+              id:=StrToInt(strngrd1.Cells[0,Arow]);
+               shoplistkek.id:=id;                     //red adres
+              shoplistkek.name:=strngrd1.Cells[1,Arow];
+              shoplistkek.adress:=InputBox('','','privet');
+              shoplistkek.tel:=strngrd1.Cells[3,Arow];
+            end;
+          3:
+            begin
+             id:=StrToInt(strngrd1.Cells[0,Arow]);
+              shoplistkek.id:=id;                     //red telefon
+              shoplistkek.name:=strngrd1.Cells[1,Arow];
+              shoplistkek.adress:=strngrd1.Cells[2,Arow];
+              shoplistkek.tel:=InputBox('','','+375296836944');
+              editShopList(shophead,id,shoplistkek);
+            end;
+          4:
+            begin
+
+            end;
+          5:
+            begin
+
+            end;
+          6:
+            begin
+             shopNum:=StrToInt(strngrd1.Cells[1,Arow]);
+             //deleteShopNum(head,shopNum);
+             end;
+        end;
 
           mode:=spr2;
          sectlistkek.id:=Random(10);
          sectlistkek.shopid:=StrToInt(strngrd1.Cells[0,Arow]);
-         sectlistkek.name=InputBox('','','kek');
-         sectlistkek.zav=InputBox('','','privetlivaya');
-         sectlistkek.tel=InputBox('','','37529235232');
+         sectlistkek.name:=InputBox('','','kek');
+         sectlistkek.zav:=InputBox('','','privetlivaya');
+         sectlistkek.tel:=InputBox('','','37529235232');
          //Отрисовать второй справочник
 
 
       end;
       spr2:
       begin
-      if Acol =2 then
-        begin
+        case Acol of
+          1:
+            begin
 
+             sectlistkek.tel:=strngrd1.Cells[3,Arow];
+             sectlistkek.zav:=strngrd1.Cells[1,Arow];
+             sectlistkek.name:=inputbox('','','kek');
+            end;
+          2:
+            begin
+             sectlistkek.tel:=inputbox('','','37529235232');
+             sectlistkek.zav:=strngrd1.Cells[1,Arow];
+             sectlistkek.name:=strngrd1.Cells[2,Arow];
+             //editShopList()
+
+            end;
+          4:
+            begin
+             sectNum:=StrToInt(strngrd1.Cells[1,Arow]);
+             //deleteSectNum(head,sectNum);
+            end;
         end;
-        if Acol = 4 then
-          sectNum:=strngrd1.Cells[1,Arow];
-          //deleteSectNum(head,sectNum);
-
-
 
       end;
     end;
