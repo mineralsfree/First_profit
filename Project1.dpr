d6919705a8bf0a1badcdee0067f1b99f8f88c971
Nikita <mineralsfree@gmail.com>
2018-04-19 14:07:55 +0300

    12
    12

--- Project1.dpr
+++ Project1.dpr
@@ -4,7 +4,9 @@ uses
   Vcl.Forms,
   OnCreate in 'OnCreate.pas' {Form1},
   Spravochnik_1 in 'Spravochnik_1.pas',
-  SplashScreen in 'SplashScreen.pas';
+  SplashScreen in 'SplashScreen.pas',
+  vedom in 'vedom.pas',
+  Vedom2 in 'Vedom2.pas';
 
 {$R *.res}
 
